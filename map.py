161f61095641446236b835a0239ef3019fc6d8e0
AyakSalu <aewyheq@gmail.com>
2024-03-10 14:38:13 +0300

    Update map.py

--- map.py
+++ map.py
@@ -2,23 +2,43 @@ from bokeh.plotting import figure,show
 from bokeh.models import WMTSTileSource
 import pandas as pd
 import numpy as np
+import xyzservices.providers as xyz
+import math
 
 coordinates = x_range, y_range = ((-13884029,-7453304),(2698291,6455972))
-p = figure(tools="wheel_zoom,pan",x_range=x_range,y_range=y_range,x_axis_type="mercator",y_axis_type="mercator")
+p = figure(tools="wheel_zoom,pan",x_range=x_range,y_range=y_range,x_axis_type="mercator",y_axis_type="mercator",height_policy="max",width_policy="max")
 url = "http://a.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png"
 attribution = "Tiles by Carto, under CC by 3.0. Data by OSM, under ODbL"
-p.add_tile(WMTSTileSource(url=url,attribution=attribution))
+
 
 def setStartingCoordinates(x1,x2,y1,y2): #BİTMEDİ
-    coordinates = x_range, y_range = ((x2-x1),(2698291,6455972))
+    x_center, y_center = wgs84_to_web_mercator2(39.9334, 32.8597) #ankara merkezli yapmak
+    x_range_start = x_center - (x_range[1] - x_range[0]) / 2
+    y_range_start = y_center - (y_range[1] - y_range[0]) / 2
+    p.x_range.start = x_range_start
+    p.x_range.end = x_range_start + (x_range[1] - x_range[0])
+    p.y_range.start = y_range_start
+    p.y_range.end = y_range_start + (y_range[1] - y_range[0])
+    p.zoom = 6
+
+
 
 def wgs84_to_web_mercator(df, lon="lon", lat="lat"):#lan lon decimaldan converter
     k = 6378137
     df["x"] = df[lon] * (k * np.pi/180.0)
     df["y"] = np.log(np.tan((90 + df[lat]) * np.pi/360.0)) * k
     return df
-      
-def haritaOlustur():  
+def wgs84_to_web_mercator2(lon="lon", lat="lat"):#lan lon decimaldan converter w.o. df
+    k = 6378137
+    x = lon * (k * np.pi/180.0)
+    y = np.log(np.tan((90 + lat) * np.pi/360.0)) * k
+    return x,y
+ 
+   
+def haritaOlustur():
+    
+    p.add_tile(WMTSTileSource(url=url,attribution=attribution))
+    
     show(p)
 def elemanEkle(lan,lon): #uçak lan lonları buna verilecek
     nycKor = 40.728333,-73.994167
@@ -26,5 +46,8 @@ def elemanEkle(lan,lon): #uçak lan lonları buna verilecek
     df = pd.DataFrame(data)
     newdf = wgs84_to_web_mercator(df)
     p.circle(x=df['x'],y=df['y'],fill_color="firebrick",size=15)
+
+setStartingCoordinates(0,0,0,0)
+
 elemanEkle(0,0)
 haritaOlustur()
