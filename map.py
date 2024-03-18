cb8c5b1ce1963a69f30dd39d63e558aef05124f1
Sabri Mert Piskin <piskinsabrimert@gmail.com>
2024-03-18 17:05:44 +0300

    map and manipulation changed2

--- map.py
+++ map.py
@@ -1,5 +1,5 @@
 from bokeh.plotting import figure, show
-from bokeh.models import WMTSTileSource, HoverTool, ColumnDataSource
+from bokeh.models import WMTSTileSource, HoverTool, ColumnDataSource,ImageURL
 import pandas as pd
 import numpy as np
 
@@ -8,7 +8,8 @@ URL = "http://a.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png"
 ATTRIBUTION = "Tiles by Carto, under CC by 3.0. Data by OSM, under ODbL"
 COORDINATES = x_range, y_range = ((-13884029, -7453304), (2698291, 6455972))
 ANKARA_COORDINATES = (39.9334, 32.8597)
-
+resim_url = "https://icons.iconarchive.com/icons/iconka/business-finance/24/paper-plane-icon.png"
+resim_url2="https://icons.iconarchive.com/icons/custom-icon-design/flatastic-10/24/Paper-plane-icon.png"
 # Create Bokeh figure
 p = figure(tools="wheel_zoom,pan", x_range=x_range, y_range=y_range,
            x_axis_type="mercator", y_axis_type="mercator",
@@ -33,17 +34,23 @@ def haritaOlustur():
     p.add_tile(WMTSTileSource(url=URL, attribution=ATTRIBUTION))
     show(p)
 
-def elemanEkle(lan, lon, planeName, velocity, altitude,isManipulated):
+def elemanEkle(lan, lon, planeName, velocity, altitude,angle,isManipulated):
     latlon = str(lan) + " ," + str(lon)
     data = dict(name=planeName, lat=[lan], lon=[lon], address=[latlon], velocity=[velocity], altitude=[altitude])
     df = pd.DataFrame(data)
-    
     newdf = wgs84_to_web_mercator(df)
     source = ColumnDataSource(newdf)
+    
+    
+    
     if(isManipulated == True):
-        p.circle(x='x', y='y', fill_color="black", size=15, source=source)
+        image1 = ImageURL(url="url", x="x", y="y",anchor="center",angle=180,angle_units='deg')
+        p.add_glyph(source, image1)
+        p.circle('x','y',source=source,fill_color='red',hover_color='yellow',size=15,fill_alpha=0,line_width=0)
     else:
-        p.circle(x='x', y='y', fill_color="red", size=15, source=source)
+        image1 = ImageURL(url="url2", x="x", y="y",anchor="center",angle=180,angle_units='deg')
+        p.add_glyph(source, image1)
+        p.circle('x','y',source=source,fill_color='red',hover_color='yellow',size=15,fill_alpha=0,line_width=0)
     hover = HoverTool(tooltips=[('Address', '@address'),('Velocity', '@velocity'),('Altitude', '@altitude')])#add plane name,velocity,altitude
     p.add_tools(hover)
     
@@ -52,6 +59,8 @@ def wgs84_to_web_mercator(df, lon="lon", lat="lat"):
     k = 6378137
     df["x"] = df[lon] * (k * np.pi / 180.0)
     df["y"] = np.log(np.tan((90 + df[lat]) * np.pi / 360.0)) * k
+    df["url"] = resim_url
+    df["url2"] = resim_url2
     return df
 
 # Set starting coordinates and display map
@@ -59,6 +68,6 @@ setStartingCoordinates(*ANKARA_COORDINATES)
 
 # Add airplane coordinates and display map
 nycKor = (40.728333, -73.994167)
-elemanEkle(*nycKor, "ucakNewYork",50,60,True)
-elemanEkle(40,-70, "ucakNewYork",50,60,False)
+elemanEkle(*nycKor, "ucakNewYork",50,60,5,True)
+elemanEkle(40,-70, "ucakNewYork",50,60,5,False)
 haritaOlustur()
